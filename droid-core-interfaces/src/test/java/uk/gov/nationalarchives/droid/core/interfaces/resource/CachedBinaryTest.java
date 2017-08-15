4e2f9e3f1076b6ad5c3c2f86eee650640840af4a
Matt Palmer <mattpalms@gmail.com>
2017-08-15 20:18:56 +0100

    Remove old commented out byteseek1 tests.
      * Note that this is really testing byteseek InputStreamReader behaviour, not DROID.
      * Byteseek 2 and 3 have complete unit tests for the InputStreamReader.
      * It's been working in the wild as well for quite some time... so maybe this test class isn't needed?

--- droid-core-interfaces/src/test/java/uk/gov/nationalarchives/droid/core/interfaces/resource/CachedBinaryTest.java
+++ droid-core-interfaces/src/test/java/uk/gov/nationalarchives/droid/core/interfaces/resource/CachedBinaryTest.java
@@ -48,46 +48,17 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * @author rflitcroft, boreilly
- * TODO: Commented out tests refer to Byteseek 1 based classes which are to be deleted folowing
- * the move to Byteseek2.  The remaining tests use the byteseek 2 InputStreamReader but we can delete
- * the entire class once the Byteseek2 test suite has been fully updated.
+ * We can delete the entire class once the Byteseek2 test suite has been fully updated.
+ * MattP: We can delete this class if you like.  It tests the behaviour of the byteseek InputStreamReader,
+ *        which is now fairly well tested - both in the wild and through more unit testing.
  *
  */
 public class CachedBinaryTest {
 
-    //private CachedByteBuffers cache;
-    
     @Before
     public void setup() {
     }
-    /*
-    @Test 
-    public void testGetInputStreamWithNoBackingFileCache() throws Exception {
-        
-        byte[] rawBytes = new byte[800];
-        new Random().nextBytes(rawBytes);
-        
-        ByteArrayInputStream in = new ByteArrayInputStream(rawBytes);
-        ByteBuffer blockZero = ByteBuffer.allocate(1000);
-        Channels.newChannel(in).read(blockZero);
-        
-        cache = new CachedByteBuffers(1, 1000, blockZero);
-        InputStreamReader reader = new InputStreamReader(in);
 
-        assertEquals(rawBytes[799], cache.readByte(799));
-
-        InputStream sourceIn = cache.getSourceInputStream();
-        
-        int byteIn;
-        int count = 0;
-        while ((byteIn = sourceIn.read()) >= 0) {
-            assertEquals("Incorrect byte: " + count, rawBytes[count], (byte) byteIn);
-            count++;
-        }
-        
-        assertEquals(800, count);
-    }
-    */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testGetInputStreamWithNoBackingFileCache1() throws Exception {
 
@@ -119,37 +90,7 @@ public class CachedBinaryTest {
         //This should throw the IndexOutOfBoundsException
         byteIn = window.getByte(rawBytes.length);
     }
-    /*
-    @Test 
-    public void testGetInputStreamWithBackingFileCache() throws Exception {
-        
-        byte[] rawBytes = new byte[8500];
-        new Random().nextBytes(rawBytes);
-        
-        ByteArrayInputStream in = new ByteArrayInputStream(rawBytes);
-        ByteBuffer blockZero = ByteBuffer.allocate(1000);
-        ReadableByteChannel ch = Channels.newChannel(in);
-        ch.read(blockZero);
-        
-        cache = new CachedByteBuffers(1, 1000, blockZero);
-        final File tempDir = new File("tmp");
-        tempDir.mkdir();
-        cache.setSourceFile(ArchiveFileUtils.writeEntryToTemp(tempDir, blockZero, ch));
-        
-        assertEquals(rawBytes[8499], cache.readByte(8499));
-        
-        InputStream sourceIn = cache.getSourceInputStream();
-        
-        int byteIn;
-        int count = 0;
-        while ((byteIn = sourceIn.read()) >= 0) {
-            assertEquals("Incorrect byte: " + count, rawBytes[count], (byte) byteIn);
-            count++;
-        }
-        
-        assertEquals(8500, count);
-    }
-    */
+
     @Ignore
     @Test(expected = IndexOutOfBoundsException.class)
     public void testGetInputStreamWithBackingFileCache1() throws Exception {
